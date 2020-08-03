eb6f98fd24cb5a3fefc5ee5b9e2c2103a926d0dd
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-08-03 16:38:40 +0530

    Updated index.js and templates

--- index.js
+++ index.js
@@ -4,22 +4,53 @@ const url = require('url');
 
 
 // Server
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+// Create a server
 const server = http.createServer((req, res) => {
     const pathName = req.url;
+
+    // Overview
     if (pathName === '/' || pathName === '/overview') {
-        res.end("This is OVERVIEW");
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCTS_CARDS%}', cardsHtml);
+        res.end(output);
+
+    // Product
     } else if (pathName === '/product') {
         res.end("This is PRODUCT");
+
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
+
+    // Not found
     } else {
         res.writeHead(404, {
-            'Content-type': 'text/html',
-            'mans-not-hot': 'something'
+            'Content-type': 'text/html'            
         });
         res.end('<h1>PAGE NOT FOUND!</h1>');
     }
