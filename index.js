ec58b76273522373b7932484b52170ca45db65aa
Sourav Singh Rawat <aidenfrostbite@gmail.com>
2020-08-04 16:41:34 +0530

    Routing is now working!

--- index.js
+++ index.js
@@ -28,10 +28,11 @@ const dataObj = JSON.parse(data);
 
 // Create a server
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    
+    const { query, pathname } = url.parse(req.url, true);
 
     // Overview
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {'Content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -39,11 +40,14 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product
-    } else if (pathName === '/product') {
-        res.end("This is PRODUCT");
+    } else if (pathname === '/product') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
 
     // API
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
 
